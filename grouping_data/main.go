87ac369c8430c629153cf7dca6d3601affcc01d6
JesusRRr <rolonjesus1996@gmail.com>
2021-01-20 20:44:07 -0600

    declaring an array

---
+++ grouping_data/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arrays
+	// An array is a building block for Slices in go
+	// Array declaration
+	// the lenght is part of the array's type
+	var myArray [6]int
+	fmt.Println(myArray)
+}
