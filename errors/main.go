1e1e137e695c8b9b684af7fa0e5270856e03d26f
JesusRRr <rolonjesus1996@gmail.com>
2021-02-20 14:22:38 -0600

    chore: using a deferred function

--- errors/main.go
+++ errors/main.go
@@ -8,10 +8,15 @@ import (
 
 func main() {
 	_, err := os.Open("no-file.txt")
+	defer foo()
 
 	if err != nil {
-		log.Fatalln("error:", err)
+		log.Print("error:", err) // this is the same that using os.Exit()
 	}
 
 	fmt.Println("Errors")
+}
+
+func foo() {
+	fmt.Println("When os.Exit() is called, Deferred functions don't run")
 }
