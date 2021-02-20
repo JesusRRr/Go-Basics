432a1fd19a7cf74eda0ccd73b5f4853a59b989d6
JesusRRr <rolonjesus1996@gmail.com>
2021-02-20 14:29:03 -0600

    chore: using panic function

--- errors/main.go
+++ errors/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
 func main() {
 	_, err := os.Open("no-file.txt")
 	defer foo()
+	defer bar()
 
 	if err != nil {
-		log.Print("error:", err) // this is the same that using os.Exit()
+		//log.Fatal("error:", err) this is the same that using os.Exit()
+		//log.Println("error:", err) this is the same that using os.Exit()
+		panic(err) // this stop the current go rutine but the deferred functions will run
 	}
 
 	fmt.Println("Errors")
@@ -19,4 +21,8 @@ func main() {
 
 func foo() {
 	fmt.Println("When os.Exit() is called, Deferred functions don't run")
+}
+
+func bar() {
+	fmt.Println("bar function deffered")
 }
