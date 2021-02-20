f49c7589224e854e749b429702d546b814219bb8
JesusRRr <rolonjesus1996@gmail.com>
2021-02-20 14:18:53 -0600

    chore: using Log.Fatal function

--- errors/main.go
+++ errors/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
+	_, err := os.Open("no-file.txt")
+
+	if err != nil {
+		log.Fatalln("error:", err)
+	}
+
 	fmt.Println("Errors")
 }
